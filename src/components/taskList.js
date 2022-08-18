5ec2b4223dc80f057b94631d8ce4f435afb9698e
Toan <votoan410@gmail.com>
2022-08-18 11:42:43 -0500

    adding more functions to control dom rendering, task list styling error fixed

--- src/components/taskList.js
+++ src/components/taskList.js
@@ -35,10 +35,10 @@ const TaskList = () => {
     return (
       <>
         <section
-          className={DomSelectors.taskEntry}
+          className={DomSelectors.taskSection.taskEntry}
           //   style={task.completed ? "text-decoration: line-through;" : null}
         >
-          <div className={DomSelectors.title} id={`div-${task.id}`}>
+          <div className={DomSelectors.taskSection.title} id={`div-${task.id}`}>
             <h3
               id={`title-click-${task.id}`}
               // style="cursor:grab"
@@ -47,7 +47,7 @@ const TaskList = () => {
             </h3>
           </div>
           <div
-            className={DomSelectors.edit}
+            className={DomSelectors.taskSection.edit}
             // style={task.completed ? "display: none" : null}
           >
             <svg
@@ -61,12 +61,15 @@ const TaskList = () => {
               <path
                 id={`btn-edit-${task.id}`}
                 fill="#ffffff"
-                d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34a.9959.9959 
-                      0 0 0-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"
+                d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34a.9959.9959
+                        0 0 0-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"
               ></path>
             </svg>
           </div>
-          <div className={DomSelectors.deleteTask} id={`btn-delete-${task.id}`}>
+          <div
+            className={DomSelectors.taskSection.deleteTask}
+            id={`btn-delete-${task.id}`}
+          >
             <svg
               focusable="false"
               aria-hidden="true"
